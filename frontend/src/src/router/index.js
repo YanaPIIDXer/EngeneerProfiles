23b759b37d14e965cf6fcc483d071c0112503c74
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-04 07:02:33 +0900

    Bootstrap-vue導入

--- frontend/src/src/router/index.js
+++ frontend/src/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Top from '@/components/Top'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(Router)
 
